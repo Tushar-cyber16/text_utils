19a48732995f61d2dbe22410c5065b0b03bd3f31
Tushar-cyber16 <ctr79302@gmail.com>
2022-02-09 12:53:50 +0530

    Updates

---
+++ precache-manifest.ecb92f136f646c99c1361cc9a78f4fc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b82428f54c7d6f2f157dae5704664f2f",
+    "url": "/text_utils/index.html"
+  },
+  {
+    "revision": "2b643a1b505921519b23",
+    "url": "/text_utils/static/css/main.16e18a48.chunk.css"
+  },
+  {
+    "revision": "19a533face9431a3281b",
+    "url": "/text_utils/static/js/2.d8b9f338.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/text_utils/static/js/2.d8b9f338.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7afe56a3e2c75e7abae",
+    "url": "/text_utils/static/js/3.7e5fe99d.chunk.js"
+  },
+  {
+    "revision": "2b643a1b505921519b23",
+    "url": "/text_utils/static/js/main.1a760cbc.chunk.js"
+  },
+  {
+    "revision": "6fe5271227fb6279918e",
+    "url": "/text_utils/static/js/runtime-main.2a14e752.js"
+  }
+]);
